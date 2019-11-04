277bd6a8821a7140886e6cc9b89b7ac4d2f9ca6e
liuyunian <sdjn_lyn@163.com>
2019-11-04 17:38:07 +0800

    fix&feat-List/ForwardList: copy ctor&operator= & push_back&pop_back

--- List/ForwardList.h
+++ List/ForwardList.h
@@ -17,23 +17,30 @@ private:
     };
 
 public:
-    ForwardList() : m_head(nullptr), m_size(0){}                                // 默认构造函数
+    ForwardList() : 
+        m_head(nullptr), 
+        m_size(0){}                                 // 默认构造函数
 
-    ForwardList(const ForwardList& other) : m_head(nullptr), m_size(0){         // 拷贝构造函数
+    ForwardList(const ForwardList& other) :         // 拷贝构造函数
+        m_head(nullptr), 
+        m_size(other.size())
+    {         
         if(other.empty()){
             return;
         }
 
         m_head = new Node(other.head()->data);
-        ++ m_size;
+        if(other.size() == 1){
+            return;
+        }
 
-        Node* selfNode = m_head->next;
+        Node* preNode = m_head;
+        Node* selfNode;
         Node* otherNode = other.head()->next;
         while(otherNode != nullptr){
             selfNode = new Node(otherNode->data);
-            ++ m_size;
-
-            selfNode = selfNode->next;
+            preNode->next = selfNode;
+            preNode = preNode->next;
             otherNode = otherNode->next;
         }
     }
@@ -77,6 +84,46 @@ public:
         -- m_size;
     }
 
+    void push_back(T elem){                         // 向链表尾部添加元素
+        Node* node = new Node(elem);
+        if(m_head == nullptr){
+            m_head = node;
+        }
+        else{
+            Node* tailNode = m_head;
+            while(tailNode->next != nullptr){
+                tailNode = tailNode->next;
+            }
+            tailNode->next = node;
+        }
+
+        ++ m_size;
+    }
+
+    void pop_back(){                                // 移除链表尾部的元素
+        if(m_head == nullptr){
+            return;
+        }
+
+        if(m_size == 1){
+            delete m_head;
+            m_head = nullptr;
+            -- m_size;
+            return;
+        }
+
+        Node* preNode = m_head;
+        Node* tailNode = preNode->next;
+        while(tailNode->next != nullptr){
+            preNode = tailNode;
+            tailNode = tailNode->next;
+        }
+        preNode->next = nullptr;
+        delete tailNode;
+
+        -- m_size;
+    }
+
     void operator=(const ForwardList& other){       // 重载赋值运算符
         Node* selfNode = m_head;
         while(selfNode != nullptr){
@@ -85,53 +132,45 @@ public:
         }
 
         m_head = nullptr;
-        m_size = 0;
+        m_size = other.size();
 
         if(other.empty()){
             return;
         }
 
         m_head = new Node(other.head()->data);
-        ++ m_size;
-
-        selfNode = m_head->next;
+        if(other.size() == 1){
+            return;
+        }
+        
+        Node* preNode = m_head;
         Node* otherNode = other.head()->next;
         while(otherNode != nullptr){
             selfNode = new Node(otherNode->data);
-            ++ m_size;
-
-            selfNode = selfNode->next;
+            preNode->next = selfNode;
+            preNode = preNode->next;
             otherNode = otherNode->next;
         }
     }
 
     // for pratice and it doesn't
-    void insert(int index, T elem){                 // 向指定位置插入元素
+    void insert(int index, T elem){}                // 向指定位置插入元素
 
-    }
+    void remove(int index){}                        // 删除指定位置的元素
 
-    void remove(int index){                         // 删除指定位置的元素
+    void set(int index, T elem){}                   // 修改指定位置的元素
 
-    }
-
-    void set(int index, T elem){                    // 修改指定位置的元素
-
-    }
-
-    T& get(int index) const {                        // 获取指定位置的元素
-
-    }
+    T& get(int index) const {}                      // 获取指定位置的元素
 
     // for unit test
-    void print() const {
-        std::cout << "ForwardList: size = " << size() << std::endl;
+    void print(const char* name) const {
+        std::cout << name << "(ForwardList): size = " << size() << '\n';
         std::cout << "data = ";
         Node* curNode = m_head;
         while(curNode != nullptr){
-            std::cout << curNode->data << " -> ";
+            std::cout << curNode->data << "->";
             curNode = curNode->next;
         }
-
         std::cout << "nullptr" << std::endl;
     }
 
--- List/ForwardList_test.cpp
+++ List/ForwardList_test.cpp
@@ -2,25 +2,35 @@
 
 int main(){
     ForwardList<int> fl;            // test default ctor
-    fl.print();
+    fl.print("fl");
 
-    for(int i = 10; i >= 0; -- i){
+    for(int i = 9; i >= 0; -- i){
         fl.push_front(i);           // test push_front
-        fl.print();
+        fl.print("fl");
     }
 
     ForwardList<int> fl2(fl);       // test copy ctor
-    fl2.print();
+    fl2.print("fl2");
 
     ForwardList<int> fl3;
     fl3.push_front(0);
     fl3.push_front(1);
     fl3 = fl;                       // test operator=
-    fl3.print();
+    fl3.print("fl3");
+
+    for(int i = 0; i < 10; ++ i){
+        fl.pop_front();             // test pop_front
+        fl.print("fl");
+    }
 
     for(int i = 0; i < 5; ++ i){
-        fl.pop_front();             // test pop_front
-        fl.print();
+        fl.push_back(i);            // test push_back
+        fl.print("fl");
+    }
+
+    for(int i = 0; i < 5; ++ i){
+        fl.pop_back();              // test pop_back
+        fl.print("fl");
     }
 
     return 0;
